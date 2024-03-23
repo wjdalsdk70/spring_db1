86992429e77542f48f63493aa9655068f822fdc0
이정민 <kyungheeyj45@ajou.ac.kr>
2024-03-24 00:53:26 +0900

    프로젝트 설정

---
+++ src/main/java/hello/spring_db1/domain/Member.java
@@ -0,0 +1,19 @@
+package hello.spring_db1.domain;
+
+import lombok.Data;
+
+@Data
+public class Member {
+
+    private String memberId;
+    private int money;
+
+    public Member(){
+
+    }
+
+    public Member(String memberId, int money) {
+        this.memberId = memberId;
+        this.money = money;
+    }
+}
---
+++ src/main/java/hello/spring_db1/service/MemberServiceV1.java
@@ -0,0 +1,28 @@
+package hello.spring_db1.service;
+
+import hello.spring_db1.domain.Member;
+import hello.spring_db1.repository.MemberRepositoryV1;
+import lombok.RequiredArgsConstructor;
+
+import java.sql.SQLException;
+
+@RequiredArgsConstructor
+public class MemberServiceV1 {
+
+    private final MemberRepositoryV1 memberRepository;
+
+    public void accountTransfer(String fromId, String toId, int money) throws SQLException {
+        Member fromMember = memberRepository.findById(fromId);
+        Member toMember = memberRepository.findById(toId);
+
+        memberRepository.update(fromId,fromMember.getMoney()-money);
+        validation(toMember);
+        memberRepository.update(toId, toMember.getMoney() + money);
+    }
+
+    private void validation(Member toMember) {
+        if (toMember.getMemberId().equals("ex")) {
+            throw new IllegalStateException("이체중 예외 발생");
+        }
+    }
+}
